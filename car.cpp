f093c9498e524b8a360e2f3d19529a90ac8e5f30
DaTunc <johannestunc@hotmail.com>
2018-11-20 11:33:40 +0100

    Few code lines

---
+++ car.cpp
@@ -0,0 +1,30 @@
+#include "car.h"
+
+
+
+void init();
+
+enum CarType get_type(){
+  return JEEP;
+}
+enum Color get_color(){
+  return RED;
+}
+
+Car get_car(CarType Car){
+  return 0;
+}
+
+double get_filled_level(Car car){
+    return 0;
+}
+void get_acceleration_rate(Car car, double rate){
+
+}
+
+void  set_acceleration_rate(Car car , double rate){
+
+}
+double get_speed(Car car){
+    return 0;
+}
--- car.h
+++ car.h
@@ -12,5 +12,19 @@
 */
 #ifndef ___CAR_H
 #define ___CAR_H
+enum CarType{AIXAM , FIAT_MULTIPLA, JEEP};
+enum Color{BLUE,RED,GREEN,BLACK,SILVER,ORANGE};
+
+typedef struct CarImplemantation* Car;
+
+void init();
+Car get_car(CarType Car);
+enum CarType get_type(Car car);
+enum Color get_color(Car car);
+double get_fill_level(Car car);
+int get_acceleration_rate(Car car);
+void  set_acceleration_rate(Car car , double rate);
+double get_speed(Car car);
+
 
 #endif
